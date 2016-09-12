93325a1ba70e1a371250c58fde8eab31ad0dbd89
oi393785 <glaucia.lemos@oi.net.br>
2016-09-12 17:30:14 -0300

    Alteração no arquivo 'webpack-dev-server'

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@
 module.exports = {
     entry: './main.js',
     output: {
-        path: './'
+        path: './',
+        filename: 'index.js'
     },
     devServer: {
         inline: true,
@@ -17,7 +18,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /node_modules/,
         loader: 'babel',
         query: {
           presets: ['es2015', 'react']
@@ -25,4 +26,4 @@ module.exports = {
       }
     ]
   }
-};
+}
