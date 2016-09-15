5e7c550a11b0e3ec2f6970d512905ef143652228
oi393785 <glaucia.lemos@oi.net.br>
2016-09-15 13:58:16 -0300

    Término da lição 3 do curso

---
+++ Lição 3/App.js
@@ -0,0 +1,23 @@
+/**
+ * Arquivo: app.js
+ * Description: arquivo responsável por renderizar os componentes. 
+ *      aqui estamos criando diferente cenários de como podemos utilizar os retornos
+ *      dos scripts com html na página
+ * Author: Glaucia Lemos
+ * Date: 15/09/2016
+ */
+
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+               <h1>Faaala Pessoal!!!</h1>
+               <b>Testando.....</b> 
+            </div>
+        );
+    }
+}
+
+export default App
--- README.md
+++ README.md
@@ -5,7 +5,8 @@ Curso de React.Js do EggHead.io onde tem por objetivo ensinar conceitos Fundamen
 Exercícios realizados durante o curso com os códigos de cada nível. :)
 
 ##### 1) [Lição 1] (https://github.com/glaucia86/curso-egghead-react/tree/master/Li%C3%A7%C3%A3o%201)
-##### 2) 
-##### 3) 
+##### 2) [Lição 2] (https://github.com/glaucia86/curso-egghead-react/tree/master/Li%C3%A7%C3%A3o%202)
+##### 3) [Lição 3]
 ##### 4) 
 ##### 5) 
+ 
